6f450fcc609d9a531286bc9567163410a23b7458
ganesh-clever <ganeshmurugan081@gmail.com>
2025-04-23 09:37:13 +0530

    test for server config

--- server.js
+++ server.js
@@ -1,64 +1,47 @@
-const arr = ["apple", "orange", "grape", "mongo", "apple"];
-const num = [1, 3,[121,232,545,454,[454,23432,565,676,78,[343433,43454,676,78,898,89],34,34,46,67,9,89],121,343,45,45,45,], 5, 7, 98, 9, 9, 0, 7, 4];
-// Filter polyfills
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { ratelimit } = require("rate-limit-express");
+require("dotenv").config();
 
-function filterHandler(data, i, array) {
-  return data === "apple";
-}
+const app = express();
 
-function myFilterHandler(cb, context) {
-  let result = [];
-  for (let i = 0; i < this.length; i++) {
-    if (cb.call(context, this[i], i, this)) {
-      result.push(this[i]);
-    }
-  }
-  return result;
-}
+const PORT = process.env.PORT || 3000;
 
-Array.prototype.myFilter = myFilterHandler;
+//Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.use(morgan("dev"));
+app.use(ratelimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
-console.log(arr.myFilter(filterHandler));
+app.use(async (req, res, next) => {
+  const versionMatch = req.path.match(/^\/api\/v(\d+)\b/);
+  console.log(versionMatch);
 
-// Map polyfills
-
-function mapHandler(data, index, array) {
-  return data * 2;
-}
-
-function myMapHandler(cb, context) {
-  let result = [];
-
-  for (let i = 0; i < this.length; i++) {
-    result.push(cb.call(context, this[i], i, this));
+  if (versionMatch) {
+    req.apiVersion = versionMatch[1];
+    return next();
   }
 
-  return result;
-}
+  res.status(400).json({
+    message: "API version not specified in URL. Use format /api/v{version}/...",
+  });
+});
 
-Array.prototype.myMap = myMapHandler;
+const getRouter = require("./router");
+app.use("/api/v1", getRouter);
 
-console.log(num.myMap(mapHandler));
-
-// Flatten array
-
-function myFlatHandler(depth = 1) {
-  let result = [];
-
-  function recursion(arr, depth) {
-    for (let i = 0; i < arr.length; i++) {
-      if (Array.isArray(arr[i]) && depth > 0) {
-        recursion(arr[i], depth - 1);
-      } else {
-        result.push(arr[i]);
-      }
-    }
+//Leasten
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server started to run with PORT: ${PORT}`);
   }
-  recursion(this,depth);
-  return result;
-}
-
-Array.prototype.myFlat = myFlatHandler;
-
-console.log(num.myFlat(4));
-
+});
